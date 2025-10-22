434069327baab61aaa2350e687eabcf391564ec6
brennanpate <pateb@oregonstate.edu>
2025-10-22 15:44:35 -0700

    update button options and make modal

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,18 +1,13 @@
 // Login Component
 'use client'
 
-import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton} from "@clerk/nextjs";
+import { SignedIn, SignedOut, SignInButton, UserButton} from "@clerk/nextjs";
 
 export default function Home() {
     return (
         <div>
             <SignedOut>
-              <SignInButton />
-              <SignUpButton>
-                <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                  Sign Up
-                </button>
-              </SignUpButton>
+              <SignInButton mode="modal"/>
             </SignedOut>
             <SignedIn>
               <UserButton />
