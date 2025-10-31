fb53ce4a0b1091bdf04ceaa8404692127c4d0e5a
brennanpate <pateb@oregonstate.edu>
2025-10-30 19:07:21 -0700

    remove unused import statement

--- src/components/ui/MobileNavbar.tsx
+++ src/components/ui/MobileNavbar.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from "react";
 import Link from "next/link";
-import { useTheme } from "next-themes";
 import { useAuth, SignInButton, SignOutButton } from "@clerk/nextjs";
 
 import {
